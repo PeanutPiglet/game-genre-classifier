d5982b0b764189f9725a40de904136c1fcb02e3d
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-16 17:01:25 -0300

    proto: initialize data preparation script

---
+++ prepare.py
@@ -0,0 +1,47 @@
+import os
+import json
+import shutil
+
+
+BATCH_SIZE = 1000
+
+
+def prepare():
+    if not verify_folder_ready():
+        return
+    
+    print(get_appdetails())
+    
+    return
+
+
+def verify_folder_ready():
+    for p in ["scraping/appdetails", "scraping/headers"]:
+        if not os.path.exists("scraping") or not os.path.isdir("scraping"):
+            print(f"ERROR: ./{p} folder not found")
+            return False
+
+    if not os.path.exists("data") or not os.path.isdir("data"):
+        print("ERROR: ./data folder not found")
+        return False
+    with os.scandir("data") as folder:
+        if any(folder):
+            print("ERROR: ./data folder is not empty  --  please remove its content")
+            return False
+    return True
+
+
+def get_appdetails():
+    dir_path = "scraping/appdetails/"
+    listed = [dir_path + f for f in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, f))]
+    listed.sort()
+    return listed
+
+
+if __name__ == "__main__":
+    prepare()
+
+
+
+
+
