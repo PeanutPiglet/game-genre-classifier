e1973a5f4010414c96c4d67962f0329e832f51c4
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-17 16:54:17 -0300

    proto: implement dense and sigmoid layer forward

--- dense.py
+++ dense.py
@@ -6,13 +6,33 @@ class NetworkDense(Network):
     layers: list[Layer]
     def __init__(self, hidden: list[int], *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+        self.layers = []
+        prev_n = 460 * 215 * 3
+        for curr_n in hidden:
+            self.layers.append(Dense(prev_n, curr_n))
+            self.layers.append(Sigmoid())
+            prev_n = curr_n
+        self.layers.append(Dense(prev_n, 100))
+        self.layers.append(Sigmoid())
+
 
 class Dense(Layer):
+
     weights: numpy.ndarray
     biases: numpy.ndarray
+    last_input: numpy.ndarray
+
     def __init__(self, n_layers_in: int, n_layers_out: int):
         self.weights = np.random.randn(n_layers_out, n_layers_in)
         self.biases = np.zeros((n_layers_out, 1))
 
+    def forward(self, inputs):
+        self.last_input = inputs
+        return self.weights @ inputs + self.biases
+
+
+class Sigmoid(Layer):
+    last_output: numpy.ndarray
+    def forward(self, inputs):
+        self.last_output = 1 / (1 + np.exp(-inputs))
 
