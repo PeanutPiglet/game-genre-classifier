ddf20556cfa650cd63239395ef6591cc36542c89
Jack Tang <jack.zzz.tang.zzz@gmail.com>
2026-07-29 22:31:00 -0300

    feat: implement saving & loading in main

--- main.py
+++ main.py
@@ -35,6 +35,41 @@ def main_loop():
                     continue
                 training.train(NETWORKS[args[0]], int(args[1]))
 
+            case 'save':
+                if len(args) < 2:
+                    print("expects at least 2 arguments (name of network, save file path)")
+                    continue
+                network = get_network(args[0])
+                if not network:
+                    print(f"Network {args[0]} not found")
+                    continue
+                network.dump(args[1])
+
+            case 'load':
+                if len(args) < 3:
+                    print("expects at least 3 arguments (savefile path, network type, network name)")
+                    continue
+                match args[1]:
+                    case 'dense':
+                        network = dense.NetworkDense(name=args[2], source=args[0], network_type='dense')
+                        if not post_network(network):
+                            print(f"Network name {network.name} already exists in current memory")
+                    case _:
+                        print(f"Invalid network type {args[1]}")
+
+
+def get_network(name: str) -> Network | None:
+    if name not in NETWORKS:
+        return None
+    return NETWORKS[name]
+
+
+def post_network(network: Network) -> bool:
+    if network.name in NETWORKS:
+        return False
+    NETWORKS[network.name] = network
+    return True
+
 
 if __name__ == "__main__":
     NETWORKS: dict[str, Network] = {}
